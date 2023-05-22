2f4633573a32c557d557cd64c4592fe9670db398
nithinK-142 <nithin.sagar.359@gmail.com>
2023-05-22 15:19:01 +0530

    password : code update.

--- password/password_generate.py
+++ password/password_generate.py
@@ -1,50 +1,57 @@
+def generate_password():
+
+    from random import sample
+
+    password = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()[]}\{|/?<>.,"
+
+    while True:
+        try:
+            pass_len = int(input("Enter Password Length: "))
+
+            # Validate the password length
+            if pass_len <= 0 or pass_len > len(password):
+                print("Invalid password length. Please enter a valid value.")
+            else:
+                # Generate a random password by sampling characters from the password string
+                generated_password = "".join(sample(password, pass_len))
+                print(f"Generated Password is : {generated_password}")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a valid integer value for password length.")
+
 def secure_password():
-    # Function to secure a password by replacing certain characters
+
     unsecured = input("Enter the Password: ")
 
     secure = (('s', '$'), ('a', '@'), ('o', '0'), ('i', '1'), ('e', '3'),
-              ('l', '|'), ('t', '+'), ('g', '9'), ('b', '8'), ('z', '2'))
+             ('l', '|'), ('t', '+'), ('g', '9'), ('b', '8'), ('z', '2'),
+             ('A', '4'), ('B', '6'), ('E', '5'), ('I', '!'), ('O', '7'))
 
-    generate = unsecured  # Initialize generate with unsecured password
+    generate = unsecured 
 
+    # Iterate over secure list and replace characters in the password
     for a, b in secure:
         generate = generate.replace(a, b)
 
+    # Check if any replacements were made
     if generate == unsecured:
         print("No Characters to Replace!!")
     else:
         print(f"Secured password is {generate}")
 
 
-def generate_password():
-    # Function to generate a random password based on user input
-    import random
-
-    password = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-
-    while True:
-        try:
-            pass_len = int(input("Enter Password Length: "))
-            if pass_len <= 0 or pass_len > len(password):
-                print("Invalid password length. Please enter a valid value.")
-            else:
-                generated_password = "".join(random.sample(password, pass_len))
-                print(f"Generated Password is : {generated_password}")
-                break
-        except ValueError:
-            print("Invalid input. Please enter a valid integer value for password length.")
-
-
 while True:
     try:
-        user_input = int(input("Enter 1 or 2: "))
+        user_input = int(input("1. Password Generation\n2. Securing Password\n3. Exit\nEnter 1, 2 or 3 : "))
 
         match user_input:
             case 1:
-                secure_password()  # Call secure_password function for securing a password
+                generate_password()
                 break
             case 2:
-                generate_password()  # Call generate_password function for generating a password
+                secure_password()
+                break
+            case 3:
                 break
 
     except ValueError:
