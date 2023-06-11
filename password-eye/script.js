e1a5dd3ee03b18395fdf9a7c91be4aed8cd4fc8b
nithinK-142 <nithin.sagar.359@gmail.com>
2023-06-11 11:19:45 +0530

    password-eye : update.

--- password-eye/script.js
+++ password-eye/script.js
@@ -1,20 +1,19 @@
-var pass = document.querySelector('#password');
-var showPass = document.querySelector('#showPassword');
-var eyeIcon = document.querySelector('#eye');
+const pass = document.querySelector('#password');
+const showPass = document.querySelector('#showPassword');
+const eyeIcon = document.querySelector('#eye');
 
 showPass.addEventListener('click', () => {
-    if (pass.getAttribute('type') === 'password' && pass.value !== '') {
-        pass.setAttribute('type', 'text');
-        eyeIcon.setAttribute('src', './view.png');
-    } 
-    else {
-        pass.setAttribute('type', 'password');
-        eyeIcon.setAttribute('src', './hide.png');
-    }
+  if (pass.value && pass.type === 'password') {
+    pass.type = 'text';
+    eyeIcon.src = './view.png';
+  } else {
+    pass.type = 'password';
+    eyeIcon.src = './hide.png';
+  }
 });
 
 pass.addEventListener('input', () => {
-    if (pass.value === '') {
-        eyeIcon.setAttribute('src', './hide.png');
-    }
+  if (!pass.value) {
+    eyeIcon.src = './hide.png';
+  }
 });
